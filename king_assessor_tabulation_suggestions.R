5e14d5a9873586e6caa2ace225c490e386b6902d
cyyllam <cyyllam@gmail.com>
2023-06-08 17:15:54 -0700

    revised comments

--- king_assessor_tabulation_suggestions.R
+++ king_assessor_tabulation_suggestions.R
@@ -4,6 +4,7 @@ data_dir <- "J:/Projects/Assessor/assessor_permit/king/data/data_research_phase"
 inputs_data_dir <- file.path(data_dir, "script_inputs")
 current_data_dir <- file.path(data_dir, "extracts/current")
 
+# names ----
 id_files <- list(pin_tran = "PIN_Translation_2021_2009.csv",
                  city_tract = "city_tract.csv",
                  city_list = "full_city_list.csv",
@@ -12,7 +13,7 @@ id_files <- list(pin_tran = "PIN_Translation_2021_2009.csv",
 bldg_files <- list(apts = "EXTR_AptComplex.csv",
                    resbldg = "EXTR_ResBldg.csv")
 
-# id files
+# id files ----
 id_dfs <- list() # initiate empty list
 
 for(i in id_files) {
@@ -22,16 +23,18 @@ for(i in id_files) {
   id_dfs[[names(which(id_files == i))]] <- df # Option 2: store file in list with names for each element
 }
 
-# building files
-
+# building files ----
 bldg_dfs <- list()
 
 for(b in bldg_files) {
   df <- read.csv(file.path(current_data_dir, b))
   names(df) <- tolower(names(df)) # lowercase columns
   df$major <- str_pad(df$major, 6, pad = 0) # padding
+  # can add in exception for condo minor with if/else...
   df$minor <- str_pad(df$minor, 4, pad = 0)
   df$pin <- paste(df$major, df$minor, sep="") # create new pin
-  # can add in exception for condo minor with if/else...
+  
   bldg_dfs[[names(which(bldg_files == b))]] <- df 
 }
+
+# 2010:2019 instead of c(2010, 2011, ..., 2019)
