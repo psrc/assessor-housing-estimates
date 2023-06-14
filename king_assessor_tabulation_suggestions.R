1ea8d9e06553535e4914b92a72a555bada341fac
cyyllam <cyyllam@gmail.com>
2023-06-14 11:50:13 -0700

    refined suggestions from code review

--- king_assessor_tabulation_suggestions.R
+++ king_assessor_tabulation_suggestions.R
@@ -4,6 +4,8 @@ data_dir <- "J:/Projects/Assessor/assessor_permit/king/data/data_research_phase"
 inputs_data_dir <- file.path(data_dir, "script_inputs")
 current_data_dir <- file.path(data_dir, "extracts/current")
 
+
+
 # names ----
 id_files <- list(pin_tran = "PIN_Translation_2021_2009.csv",
                  city_tract = "city_tract.csv",
@@ -13,15 +15,30 @@ id_files <- list(pin_tran = "PIN_Translation_2021_2009.csv",
 bldg_files <- list(apts = "EXTR_AptComplex.csv",
                    resbldg = "EXTR_ResBldg.csv")
 
+
+
+
+
+
+
 # id files ----
 id_dfs <- list() # initiate empty list
 
-for(i in id_files) {
-  df <- read.csv(file.path(inputs_data_dir, i))
+for(id_file in id_files) {
+  df <- read.csv(file.path(inputs_data_dir, id_file))
   names(df) <- tolower(names(df)) # set column names to lowercase
   # id_dfs[[i]] <- df # Option 1: store file in list
-  id_dfs[[names(which(id_files == i))]] <- df # Option 2: store file in list with names for each element
+  id_dfs[[names(which(id_files == id_file))]] <- df # Option 2: store file in list with names for each element
 }
+
+head(id_dfs$pin_tran)
+
+
+
+
+
+
+
 
 # building files ----
 bldg_dfs <- list()
@@ -32,7 +49,7 @@ for(b in bldg_files) {
   df$major <- str_pad(df$major, 6, pad = 0) # padding
   # can add in exception for condo minor with if/else...
   df$minor <- str_pad(df$minor, 4, pad = 0)
-  df$pin <- paste(df$major, df$minor, sep="") # create new pin
+  df$pin <- paste0(df$major, df$minor) # create new pin
   
   bldg_dfs[[names(which(bldg_files == b))]] <- df 
 }
