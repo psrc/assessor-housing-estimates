61a2b6811c109f3d7b4fcd3571eafcd48193a3f6
grantmgibson <53488250+grantmgibson@users.noreply.github.com>
2024-10-01 15:13:15 -0700

    updated pierce: file paths, project year, psrcelmer

--- process_assessor_pierce.R
+++ process_assessor_pierce.R
@@ -4,19 +4,12 @@ library(writexl)
 library(odbc)
 library(DBI)
 library(sf)
+library(psrcelmer)
 
 # Define file paths and other variables -------------------------------------------------------
 
-# ElmerGeo db connection
-geo_conn <- dbConnect(odbc::odbc(),
-                      driver = "ODBC Driver 17 for SQL Server",
-                      server = "AWS-PROD-SQL\\Sockeye",
-                      database = "ElmerGeo",
-                      trusted_connection = "yes"
-)
-
 # source data file paths
-current_file_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/2023/extracts/current/"
+current_file_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/2024/extracts/"
 current_appraisal_file_name <- "appraisal_account.txt"
 current_improvement_file_name <- "improvement.txt"
 current_builtas_file_name <- "improvement_builtas.txt"
@@ -26,34 +19,32 @@ base_appraisal_file_name <- "appraisal_account_2012.csv"
 base_improvement_file_name <- "improvement_2012.csv"
 base_builtas_file_name <- "improvement_builtas_2012.csv"
 
-current_shapefile_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/2023/GIS/"
-current_base_shapefile_name <- "parcels_2023_2012_region22_tract20.shp"
+current_shapefile_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/2024/GIS/"
+current_base_shapefile_name <- "parcels_2024_2012_region23_tract20.shp"
 
 base_shapefile_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/base_year/GIS/"
 condo_base_shapefile_name <- "pierce_condos_2012.shp"
 
-juris_query <- "SELECT juris, feat_type FROM ElmerGeo.dbo.PSRC_REGION WHERE cnty_name = 'Pierce' AND feat_type <> 'water'"
+juris_query <- "SELECT juris, feat_type FROM dbo.PSRC_REGION WHERE cnty_name = 'Pierce' AND feat_type <> 'water'"
 
-tract_query <- "SELECT geoid20 FROM ElmerGeo.dbo.TRACT2020 WHERE county_name = 'Pierce'"
+tract_query <- "SELECT geoid20 FROM dbo.TRACT2020 WHERE county_name = 'Pierce'"
 
-output_file_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/2023/script_outputs/"
+output_file_path <- "J:/Projects/Assessor/assessor_permit/pierce/data/2024/script_outputs/"
 
 year_start <- 2012
-year_end <- 2022
+year_end <- 2023
+proj_year <- 2024
 
 
 # Load data from source -----------------------------------------------------------------------
 
-juris <- dbGetQuery(geo_conn, juris_query) %>% 
+juris <- get_query(sql = juris_query, db_name = "ElmerGeo") %>% 
   mutate(juris = ifelse(feat_type %in% c("uninc", "rural"), "Unincorporated Pierce", juris)) %>% 
   select(-feat_type) %>% 
   distinct() %>% 
-  arrange()
+  arrange(juris)
 
-tracts <- dbGetQuery(geo_conn, tract_query)
-
-dbDisconnect(geo_conn)
-rm(geo_conn)
+tracts <- get_query(sql = tract_query, db_name = "ElmerGeo")
 
 current_improvement <- read_delim(paste0(current_file_path, current_improvement_file_name),
                                   delim = "|",
@@ -101,15 +92,15 @@ base_appraisal <- read_csv(paste0(base_file_path, base_appraisal_file_name),
                              parcel_number = col_character()
                            ))
 
-# Read in current year base parcel shapefile with 2012 PINs
-# This is created in ArcMap prior to R processing, using psrc_region layer
+# Read in current year base parcel shapefile with base parcel PINs
+# This is created using the parcel preprocessing python script
 parcels_current_base <- st_read(paste0(current_shapefile_path, current_base_shapefile_name),
                              crs = 2285, stringsAsFactors = FALSE) %>% 
   rename(current_prcl = taxparceln) %>% 
   mutate(juris = ifelse(juris == "Dupont", "DuPont", juris))
 
 # Read in base year condo parcel shapefile with base parcel PINs
-# This is created in ArcMap prior to R processing
+# This was created in ArcMap prior to R processing
 condo_parcels_base <- st_read(paste0(base_shapefile_path, condo_base_shapefile_name),
                               crs = 2285, stringsAsFactors = FALSE)
 
@@ -592,7 +583,7 @@ write_xlsx(x = split(tract_units, tract_units$year_built) %>% map(., ~ (.x %>% s
 # Create output for combined region process ---------------------------------------------------
 # parcel table for shapefile
 parcel_new <- current_base_join %>% 
-  mutate(project_year = 2023,
+  mutate(project_year = proj_year,
          county = "Pierce",
          county_fips = "053") %>% 
   select(project_year,
@@ -611,7 +602,7 @@ parcel_new <- current_base_join %>%
 
 parcel_demo <- demos %>% 
   filter(demo_units != 0) %>% 
-  mutate(project_year = 2023,
+  mutate(project_year = proj_year,
          county = "Pierce",
          county_fips = "053",
          development = "demolition") %>% 
@@ -636,7 +627,7 @@ pierce_county_units_long <- county_units %>%
   pivot_longer(cols = net_total:`mobile homes`,
                names_to = "structure_type",
                values_to = "net_units") %>% 
-  mutate(project_year = 2023, 
+  mutate(project_year = proj_year, 
          county = "Pierce") %>% 
   select(project_year, county, year = year_built, structure_type, net_units)
 
@@ -644,7 +635,7 @@ pierce_juris_units_long <- juris_units %>%
   pivot_longer(cols = net_total:`mobile homes`,
                names_to = "structure_type",
                values_to = "net_units") %>% 
-  mutate(project_year = 2023, 
+  mutate(project_year = proj_year, 
          county = "Pierce") %>% 
   select(project_year, county, juris, year = year_built, structure_type, net_units)
 
@@ -652,10 +643,10 @@ pierce_tract_units_long <- tract_units %>%
   pivot_longer(cols = net_total:`mobile homes`,
                names_to = "structure_type",
                values_to = "net_units") %>% 
-  mutate(project_year = 2023, 
+  mutate(project_year = proj_year, 
          county = "Pierce") %>% 
   select(project_year, county, tract = tractid, year = year_built, structure_type, net_units)
 
 # save tables to .rda for combining script
 save(pierce_parcel_tbl, pierce_county_units_long, pierce_juris_units_long, pierce_tract_units_long,
-     file = "J:/Projects/Assessor/assessor_permit/data_products/2023/elmer/pierce_tables.rda")
+     file = "J:/Projects/Assessor/assessor_permit/data_products/2024/elmer/pierce_tables.rda")
